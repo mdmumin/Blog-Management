9b17d9cb0d7de4f306946b6cce145a3fced90f0b
mdmumin <mdmuminjap@gmail.com>
2024-11-24 00:59:00 +0600

    New Change: addpost

--- admin/addpost.php
+++ admin/addpost.php
@@ -3,8 +3,27 @@
 <div class="grid_10">
     <div class="box round first grid">
         <h2>Add New Post</h2>
+        <?php 
+           if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                $title = mysqli_real_escape_string($db->link, $_POST['title']);
+                $cat = mysqli_real_escape_string($db->link, $_POST['cat']);
+                $body = mysqli_real_escape_string($db->link, $_POST['body']);
+                $tags = mysqli_real_escape_string($db->link, $_POST['tags']);
+                $author = mysqli_real_escape_string($db->link, $_POST['author']);
+
+                $permited  = array('jpg', 'jpeg', 'png', 'gif');
+                $file_name = $_FILES['image']['name'];
+                $file_size = $_FILES['image']['size'];
+                $file_temp = $_FILES['image']['tmp_name'];
+
+                $div = explode('.', $file_name);
+                $file_ext = strtolower(end($div));
+                $unique_image = substr(md5(time()), 0, 10).'.'.$file_ext;
+                $uploaded_image = "upload/".$unique_image;
+            }
+        ?>
         <div class="block">
-            <form action="" method="" enctype="multipart/form-data">
+            <form action="" method="post" enctype="multipart/form-data">
                 <table class="form">
 
                     <tr>
@@ -12,7 +31,7 @@
                             <label>Title</label>
                         </td>
                         <td>
-                            <input type="text" placeholder="Enter Post Title..." class="medium" />
+                            <input type="text" name="title" placeholder="Enter Post Title..." class="medium" />
                         </td>
                     </tr>
 
@@ -21,21 +40,18 @@
                             <label>Category</label>
                         </td>
                         <td>
-                            <select id="select" name="select">
-                                <option value="1">Category One</option>
-                                <option value="2">Category Two</option>
-                                <option value="3">Cateogry Three</option>
+                            <select id="select" name="cat">
+                                <option value="1">Select Category</option>
+                                <?php
+                                    $query = "select * from tbl_category";
+                                    $category = $db->select($query);
+                                    if ($category) {
+                                        while ($result = $category->fetch_assoc()) {
+                                            ?>
+                                <option value="<?php echo $result['id']; ?>"><?php echo $result['name']; ?></option>
+                                <?php  } }    ?>
+
                             </select>
-                        </td>
-                    </tr>
-
-
-                    <tr>
-                        <td>
-                            <label>Date Picker</label>
-                        </td>
-                        <td>
-                            <input type="text" id="date-picker" />
                         </td>
                     </tr>
                     <tr>
@@ -43,7 +59,7 @@
                             <label>Upload Image</label>
                         </td>
                         <td>
-                            <input type="file" />
+                            <input type="file" name="image" />
                         </td>
                     </tr>
                     <tr>
@@ -51,7 +67,23 @@
                             <label>Content</label>
                         </td>
                         <td>
-                            <textarea class="tinymce"></textarea>
+                            <textarea class="tinymce" name="body"></textarea>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>Tags</label>
+                        </td>
+                        <td>
+                            <input type="text" name="tags" placeholder="Enter Post Tags..." class="medium" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>Author</label>
+                        </td>
+                        <td>
+                            <input type="text" name="author" placeholder="Enter Post author..." class="medium" />
                         </td>
                     </tr>
                     <tr>
